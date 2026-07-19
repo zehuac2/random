bf761d0cc11b93e22c501928b22e0319eb08cae3
Zehua Chen <zehua-chen@outlook.com>
2026-07-19 00:13:58 -0700

    Configure Rolldown bundle splitting

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,25 @@ import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    rolldownOptions: {
+      output: {
+        entryFileNames: 'app-[hash].js',
+        codeSplitting: {
+          groups: [
+            {
+              name: 'mui-emotion',
+              test: /node_modules[\/](@mui|@emotion)[\/]/,
+              priority: 2,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules[\/]/,
+              priority: 1,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
